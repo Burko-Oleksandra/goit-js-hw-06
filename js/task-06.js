cc189f70c9363ed48be7f2d8500373f735e427e1
Oleksandra <sashkaburko1994@gmail.com>
2022-10-03 23:58:03 +0300

    add 6 and 7 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,4 +1,32 @@
-// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
-// Яка кількість смиволів повинна бути в інпуті, зазначається в його атрибуті data-length.
-// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
-// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
+// Напиши скрипт, який
+
+// 1. під час втрати фокусу на інпуті
+// (подія blur), перевіряє його вміст щодо правильної
+// кількості введених символів.
+
+// 2. Яка кількість смиволів повинна бути в інпуті,
+// зазначається в його атрибуті data - length.
+//
+// 3. Якщо введена правильна кількість символів,
+// то border інпуту стає зеленим, якщо неправильна
+// кількість - червоним.
+
+// 4. Для додавання стилів використовуй CSS-класи
+// valid і invalid, які ми вже додали у вихідні файли завдання.
+
+const formInput = document.querySelector('#validation-input');
+// console.log(formInput);
+
+const inputLength = formInput.dataset.length;
+// console.log(inputLength);
+
+formInput.addEventListener('blur', () => {
+  //   console.log(formInput.value.length);
+  if (formInput.value.length == inputLength) {
+    formInput.classList.remove('invalid');
+    formInput.classList.add('valid');
+  } else {
+    formInput.classList.remove('valid');
+    formInput.classList.add('invalid');
+  }
+});
--- js/task-07.js
+++ js/task-07.js
@@ -1,3 +1,29 @@
-// Напиши скрипт, який реагує на зміну значення input#font-size-control
-// (подія input) і змінює інлайн - стиль span#text, оновлюючи властивість
-// font - size.В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
+// Напиши скрипт, який реагує на зміну значення
+// input#font - size - control (подія input) і змінює
+// інлайн - стиль span#text, оновлюючи властивість font - size.
+// В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
+
+const formInput = document.querySelector('#font-size-control');
+// console.log(formInput);
+
+const spanText = document.querySelector('#text');
+// console.log(spanText);
+
+// formInput.value = 20;
+// console.log(formInput.value);
+
+// 1 варіант
+formInput.addEventListener('input', () => {
+  console.log(spanText.style.fontSize);
+  spanText.style.fontSize = `${formInput.value}px`;
+});
+
+// 2 варіант
+// formInput.addEventListener('blur', () => {
+//   spanText.style.fontSize = `${formInput.value}px`;
+// });
+
+// 3 варіант
+// formInput.addEventListener('mouseup', () => {
+//   spanText.style.fontSize = `${formInput.value}px`;
+// });
